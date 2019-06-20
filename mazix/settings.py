a56e02e3f6e63652335b291d23e0b4063af0ce96
nirojhmg <nirojhmg@gmail.com>
2019-06-20 21:58:54 +0545

    Setting Static Root and Dir added

--- mazix/settings.py
+++ mazix/settings.py
@@ -119,11 +119,18 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "media"),
+    '/var/www/static/',
+]
 
 # Activate Django-heroku.
 django_heroku.settings(locals())
