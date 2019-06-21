87194bd74dfee1a96b2fd268576ddfab001296d4
nirojhmg <nirojhmg@gmail.com>
2019-06-21 08:33:46 +0545

    STATICFILES_DIRS changes

--- mazix/settings.py
+++ mazix/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'media')
-STATIC_URL = '/media/'
+STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
@@ -134,3 +134,5 @@ STATICFILES_DIRS = [
 
 # Activate Django-heroku.
 django_heroku.settings(locals())
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
