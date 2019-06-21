870cec86d9e6d2b6b42ea6705e297460ee832fbd
nirojhmg <nirojhmg@gmail.com>
2019-06-21 08:10:21 +0545

    STATICFILES_DIRS changes

--- mazix/settings.py
+++ mazix/settings.py
@@ -128,7 +128,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "media")
+    'http://mazix.herokuapp.com/media/',
+
 ]
 
 # Activate Django-heroku.
