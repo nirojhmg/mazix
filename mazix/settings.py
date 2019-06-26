48c42fb66a66210575362cb11e1fde8eb95663b8
nirojhmg <nirojhmg@gmail.com>
2019-06-26 16:12:56 +0545

    Setting changes for media

--- mazix/settings.py
+++ mazix/settings.py
@@ -119,8 +119,18 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_index')
+STATICFILES_DIRS = (
+    os.path.join(
+        BASE_DIR,
+        'static',
+    ),
+)
+
 STATIC_URL = '/static/'
+LOGIN_REDIRECT_URL = '/profile'
+MEDIA_URL='/media/'
+MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
